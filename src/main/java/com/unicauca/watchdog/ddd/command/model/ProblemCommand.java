75b1b63d8c7424dafc6b13b8a048eb733bdb6c31
whastica <drlook92@gmail.com>
2024-09-11 09:10:09 -0500

    estructura CQRS

---
+++ src/main/java/com/unicauca/watchdog/ddd/command/model/ProblemCommand.java
@@ -0,0 +1,17 @@
+package com.unicauca.watchdog.ddd.command.model;
+
+import jakarta.annotation.Priority;
+
+public class ProblemCommand {
+    /*  Persistir un nuevo problema
+    public void save(Problema problema) {
+        entityManager.persist(problema);
+    }
+
+     Actualizar la prioridad de un problema
+    public void updatePriority(Long problemaId, Priority newPriority) {
+        Problema problema = entityManager.find(Problema.class, problemaId);
+        problema.setPriority(newPriority);
+        entityManager.merge(problema);
+    }*/
+}
