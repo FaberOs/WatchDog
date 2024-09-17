6249bd50a583c43a0eeac546cd6086cbe4a518d3
whastica <drlook92@gmail.com>
2024-09-17 02:14:26 -0500

    Primmera implementacion de la Sincronizacion

--- src/main/java/com/unicauca/watchdog/ddd/sync/controller/syncController.java
+++ src/main/java/com/unicauca/watchdog/ddd/sync/controller/syncController.java
@@ -1,7 +1,20 @@
 package com.unicauca.watchdog.ddd.sync.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class syncController {
+import com.unicauca.watchdog.ddd.sync.service.SyncService;
 
+@RestController
+public class SyncController {
+
+    @Autowired
+    private SyncService syncService;
+  
+    @PostMapping("/sync")
+    public void sync() {
+      syncService.sync();
+    }
+    
 }
--- src/main/java/com/unicauca/watchdog/ddd/sync/service/syncService.java
+++ src/main/java/com/unicauca/watchdog/ddd/sync/service/syncService.java
@@ -1,7 +1,22 @@
 package com.unicauca.watchdog.ddd.sync.service;
+
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class syncService {
+public class SyncService {
+    Date lastSyncDate = new Date();
 
+    @Autowired
+    private MongoOperations mongoOps;
+
+    public void sync() {
+        Date newSyncDate = new Date();
+        lastSyncDate = newSyncDate;
+    }
 }
