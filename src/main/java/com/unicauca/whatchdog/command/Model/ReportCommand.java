59995aed594e368c31862dc1ffab9731dba97f18
Carsolarte <carsolarte@unicauca.edu.co>
2024-09-23 18:01:44 -0500

    Ajuste model

--- src/main/java/com/unicauca/whatchdog/command/Model/ReportCommand.java
+++ src/main/java/com/unicauca/whatchdog/command/Model/ReportCommand.java
@@ -2,8 +2,6 @@ package com.unicauca.whatchdog.command.Model;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 @Entity
@@ -11,8 +9,7 @@ import lombok.Data;
 public class ReportCommand {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     private String content;
     //private CoordenadasUbiacion coordenadas;
     private LocalDateTime fechaCreacion;
